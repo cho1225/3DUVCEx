83034ec403c04de3a43f40611260789855985152
yuyan971 <pierretree.02@gmail.com>
2024-06-28 15:44:51 +0900

    test2

--- Assets/HumanCtrl_base.cs
+++ Assets/HumanCtrl_base.cs
@@ -32,7 +32,7 @@ public class HumanCtrl_base : MonoBehaviour{
     }
     void Update()
     {
-        Debug.Log("test1");
+        Debug.Log("test2");
         Move_Ctrl();
     }
     void Move_Ctrl()
